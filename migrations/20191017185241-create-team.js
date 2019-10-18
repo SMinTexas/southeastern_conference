dfdcf3a3dfdc0844d566fa8cb31c77b9a23e8dfc
Steven Murray <tstevenm@gmail.com>
2019-10-18 07:42:19 -0500

    Updates to the sequelize DB, data presentation

--- migrations/20191017185241-create-team.js
+++ migrations/20191017185241-create-team.js
@@ -1,30 +1,57 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Teams', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      division: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Teams');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Teams', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                type: Sequelize.STRING
+            },
+            division: {
+                type: Sequelize.STRING
+            },
+            nickname: {
+                type: Sequelize.STRING
+            },
+            mascot: {
+                type: Sequelize.STRING
+            },
+            location: {
+                type: Sequelize.STRING
+            },
+            stadium: {
+                type: Sequelize.STRING
+            },
+            capacity: {
+                type: Sequelize.STRING
+            },
+            dTitles: {
+                type: Sequelize.STRING
+            },
+            cTitles: {
+                type: Sequelize.STRING
+            },
+            nTitles: {
+                type: Sequelize.STRING
+            },
+            teamLogo: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Teams');
+    }
 };
--- models/team.js
+++ models/team.js
@@ -1,11 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Team = sequelize.define('Team', {
-    name: DataTypes.STRING,
-    division: DataTypes.STRING
-  }, {});
-  Team.associate = function(models) {
-    // associations can be defined here
-  };
-  return Team;
+    const Team = sequelize.define('Team', {
+        name: DataTypes.STRING,
+        division: DataTypes.STRING,
+        nickname: DataTypes.STRING,
+        mascot: DataTypes.STRING,
+        location: DataTypes.STRING,
+        stadium: DataTypes.STRING,
+        capacity: DataTypes.STRING,
+        dTitles: DataTypes.STRING,
+        cTitles: DataTypes.STRING,
+        nTitles: DataTypes.STRING,
+        teamLogo: DataTypes.STRING
+    }, {});
+    Team.associate = function(models) {
+        // associations can be defined here
+    };
+    return Team;
 };
